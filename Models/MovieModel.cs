4b9eb4d2e046ce4a55b303d81feb775aafef6148
Wannapa Srijermtong <wannapaa_s@hotmail.com>
2020-07-02 12:00:12 +0700

    Insert data to database and Validation Form

--- Models/MovieModel.cs
+++ Models/MovieModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +9,12 @@ namespace TrainingDotNetCoreMVC.Models
     public class MovieModel
     {
         public int id { get; set; }
+        [Required]
         public string title { get; set; }
         public string coverImg { get; set; }
+        [Required]
         public DateTime? releaseDate { get; set; }
+        [Required]
         public string genre { get; set; }
         public int duration { get; set; }
         public DateTime? createDate { get; set; }
