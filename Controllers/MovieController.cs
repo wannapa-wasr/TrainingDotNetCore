96641c3da50862d9f47dcce5a751fd4afc9a206b
Wannapa Srijermtong <wannapaa_s@hotmail.com>
2020-07-02 09:36:56 +0700

    Add project files.

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TrainingDotNetCoreMVC.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Welcome(string name, string id)
+        {
+            return $"Hello, Your name's {name}. Your ID is {id}.";
+        }
+    }
+}
